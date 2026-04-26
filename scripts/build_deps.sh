2a45a9213a0cce38281eaa04fb04aca610a482b0
Noah Cohn <noahc42@gmail.com>
2026-04-26 05:06:34 -0700

    Update build_deps.sh

--- scripts/build_deps.sh
+++ scripts/build_deps.sh
@@ -4,6 +4,6 @@
 ./build.hwloc.sh
 ./build.openssl3.sh
 
-/content/build_space/xmrig_wasm/build.uv.sh
-/content/build_space/xmrig_wasm/build.hwloc.sh
-/content/build_space/xmrig_wasm/build.openssl3.sh
+/content/build_space/xmrig_wasm/scripts/build.uv.sh
+/content/build_space/xmrig_wasm/scripts/build.hwloc.sh
+/content/build_space/xmrig_wasm/scripts/build.openssl3.sh
